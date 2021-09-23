5324372fe2a52c507c4929779679bd4632c52d24
Sasitha Kumarasinghe <sasithathathsara@outlook.com>
2021-09-23 11:44:56 +0530

    Added sumOfprimes(n)

--- src/main/java/it/uom/cse/MathOperation.java
+++ src/main/java/it/uom/cse/MathOperation.java
@@ -15,4 +15,25 @@ public class MathOperation {
         return operand1 - operand2;
     }
 
+    public static int sumOfPrimes(int n) {
+        int cnt = 0;
+        int sum = 0;
+        boolean flag;
+        for (int i = 2; ; ++i) {
+            flag = true;
+            for (int j = 2; j <= i / 2; ++j) {
+                if (i %j == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                sum += i;
+                cnt++;
+                if(cnt > n) break;
+            }
+        }
+        return sum;
+    }
+
 }
